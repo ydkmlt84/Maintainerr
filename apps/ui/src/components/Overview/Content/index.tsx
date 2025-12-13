9353d89720fcad5d1f3c8aed6336724fa62b0e42
ydkmlt84 <justin@tacohouse.us>
2025-12-12 23:46:16 -0600

    refactor: Update IPlexMetadata import paths and add OverviewPoster component

--- apps/ui/src/components/Overview/Content/index.tsx
+++ apps/ui/src/components/Overview/Content/index.tsx
@@ -1,10 +1,10 @@
 import { debounce } from 'lodash-es'
 import { useEffect } from 'react'
-import { ICollectionMedia } from '../../Collection'
-import LoadingSpinner, {
-  SmallLoadingSpinner,
-} from '../../Common/LoadingSpinner'
-import MediaCard from '../../Common/MediaCard'
+import type { ICollectionMedia } from '../../Collection'
+import LoadingSpinner from '../../Common/LoadingSpinner'
+import OverviewPoster from '../PosterView'
+import OverviewTable from '../TableView'
+import type { IPlexMetadata } from '../iPlexMetadata'
 
 interface IOverviewContent {
   data: IPlexMetadata[]
@@ -17,60 +17,7 @@ interface IOverviewContent {
   collectionPage?: boolean
   collectionInfo?: ICollectionMedia[]
   collectionId?: number
-}
-
-export interface IPlexMetadata {
-  ratingKey: string
-  key: string
-  parentRatingKey?: string
-  grandparentRatingKey?: string
-  art: string
-  audienceRating?: number
-  audienceRatingImage?: string
-  contentRating?: string
-  duration: number
-  guid: string
-  type: 'movie' | 'show' | 'season' | 'episode'
-  title: string
-  Guid: {
-    id: string
-  }[]
-  Genre?: {
-    id: string
-  }[]
-  Country?: {
-    tag: string
-  }[]
-  Role?: {
-    tag: string
-  }[]
-  Writer?: {
-    tag: string
-  }[]
-  Director?: {
-    tag: string
-  }[]
-  addedAt: number
-  childCount?: number
-  leafCount?: number
-  viewedLeafCount?: number
-  primaryExtraKey: string
-  originallyAvailableAt: string
-  updatedAt: number
-  thumb: string
-  tagline?: string
-  summary: string
-  studio: string
-  year: number
-  parentTitle?: string
-  grandparentTitle?: string
-  parentData?: IPlexMetadata
-  parentYear?: number
-  grandParentYear?: number
-  index?: number
-  maintainerrExclusionType?: 'specific' | 'global' // this is added by Maintainerr, not a Plex type
-  maintainerrExclusionId?: number // this is added by Maintainerr, not a Plex type
-  maintainerrIsManual?: boolean // this is added by Maintainerr, not a Plex type
+  viewMode: 'poster' | 'table'
 }
 
 const OverviewContent = (props: IOverviewContent) => {
@@ -90,7 +37,7 @@ const OverviewContent = (props: IOverviewContent) => {
     window.addEventListener('scroll', debouncedScroll, { passive: true })
     return () => {
       window.removeEventListener('scroll', debouncedScroll)
-      debouncedScroll.cancel() // Cancel pending debounced calls
+      debouncedScroll.cancel()
     }
   }, [])
 
@@ -107,121 +54,46 @@ const OverviewContent = (props: IOverviewContent) => {
   }, [props.data])
 
   const getDaysLeft = (plexId: number) => {
-    if (props.collectionInfo) {
-      const collectionData = props.collectionInfo.find(
-        (colEl) => colEl.plexId === +plexId,
-      )
-      if (collectionData && collectionData.collection) {
-        if (collectionData.collection.deleteAfterDays == null) {
-          return undefined
-        }
+    if (!props.collectionInfo) return undefined
 
-        const date = new Date(collectionData.addDate)
-        const today = new Date()
+    const collectionData = props.collectionInfo.find(
+      (colEl) => colEl.plexId === +plexId,
+    )
 
-        date.setDate(date.getDate() + collectionData.collection.deleteAfterDays)
+    if (!collectionData?.collection) return undefined
+    if (collectionData.collection.deleteAfterDays == null) return undefined
 
-        const diffTime = date.getTime() - today.getTime()
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
-        return diffDays
-      }
-    }
-    return undefined
+    const date = new Date(collectionData.addDate)
+    const today = new Date()
+
+    date.setDate(date.getDate() + collectionData.collection.deleteAfterDays)
+
+    const diffTime = date.getTime() - today.getTime()
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    return diffDays
   }
 
-  if (props.loading) {
-    return <LoadingSpinner />
-  }
+  if (props.loading) return <LoadingSpinner />
+  if (!props.data || props.data.length === 0) return null
 
-  if (props.data && props.data.length > 0) {
+  if (props.viewMode === 'table') {
     return (
-      <ul className="cards-vertical">
-        {props.data.map((el) => (
-          <li key={+el.ratingKey}>
-            <MediaCard
-              id={+el.ratingKey}
-              libraryId={props.libraryId}
-              type={
-                el.type === 'movie'
-                  ? 1
-                  : el.type === 'show'
-                    ? 2
-                    : el.type === 'season'
-                      ? 3
-                      : 4
-              }
-              image={''}
-              summary={
-                el.type === 'movie' || el.type === 'show'
-                  ? el.summary
-                  : el.type === 'season'
-                    ? el.title
-                    : el.type === 'episode'
-                      ? 'Episode ' + el.index + ' - ' + el.title
-                      : ''
-              }
-              year={
-                el.type === 'episode'
-                  ? el.parentTitle
-                  : el.parentYear
-                    ? el.parentYear.toString()
-                    : el.year?.toString()
-              }
-              mediaType={
-                el.type === 'movie'
-                  ? 'movie'
-                  : el.type === 'show'
-                    ? 'show'
-                    : el.type === 'season'
-                      ? 'season'
-                      : 'episode'
-              }
-              title={
-                el.grandparentTitle
-                  ? el.grandparentTitle
-                  : el.parentTitle
-                    ? el.parentTitle
-                    : el.title
-              }
-              userScore={el.audienceRating ? el.audienceRating : 0}
-              exclusionId={
-                el.maintainerrExclusionId
-                  ? el.maintainerrExclusionId
-                  : undefined
-              }
-              tmdbid={
-                el.parentData
-                  ? el.parentData.Guid?.find((e) =>
-                      e.id?.includes('tmdb'),
-                    )?.id?.split('tmdb://')[1]
-                  : el.Guid
-                    ? el.Guid.find((e) => e.id?.includes('tmdb'))?.id?.split(
-                        'tmdb://',
-                      )[1]
-                    : undefined
-              }
-              collectionPage={
-                props.collectionPage ? props.collectionPage : false
-              }
-              exclusionType={el.maintainerrExclusionType}
-              onRemove={props.onRemove}
-              collectionId={props.collectionId}
-              isManual={el.maintainerrIsManual ? el.maintainerrIsManual : false}
-              {...(props.collectionInfo
-                ? {
-                    daysLeft: getDaysLeft(+el.ratingKey),
-                    collectionId: props.collectionInfo.find(
-                      (colEl) => colEl.plexId === +el.ratingKey,
-                    )?.collectionId,
-                  }
-                : undefined)}
-            />
-          </li>
-        ))}
-        {props.extrasLoading ? <SmallLoadingSpinner /> : undefined}
-      </ul>
+      <OverviewTable data={props.data} extrasLoading={props.extrasLoading} />
     )
   }
-  return <></>
+
+  return (
+    <OverviewPoster
+      data={props.data}
+      libraryId={props.libraryId}
+      extrasLoading={props.extrasLoading}
+      collectionPage={props.collectionPage}
+      collectionInfo={props.collectionInfo}
+      collectionId={props.collectionId}
+      onRemove={props.onRemove}
+      getDaysLeft={getDaysLeft}
+    />
+  )
 }
+
 export default OverviewContent
